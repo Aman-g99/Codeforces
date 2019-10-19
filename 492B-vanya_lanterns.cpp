864d1523c3003a0283f36d551544278eca0f21e6
Aman Garg <32516081+Aman-g99@users.noreply.github.com>
2019-10-19 12:23:04 +0530

    update

--- 492B-vanya_lanterns.cpp
+++ 492B-vanya_lanterns.cpp
@@ -2,5 +2,31 @@
 using namespace std;
 
 int main(){
-	
+	int n;
+	double l;
+	cin>>n>>l;
+	double ln[n];
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>ln[i];
+	}
+
+	sort(ln,ln+n);
+
+	// for (int i = 0; i < n; ++i)
+	// {
+	// 	cout<<ln[i]<<"*";
+	// }
+
+	double d = max(ln[0]-0,l-ln[n-1]);
+	for (int i = 0; i < n-1; ++i)
+	{
+		d = max(d,((ln[i+1]-ln[i])/2.0));
+	}
+	cout.precision(10);
+
+	cout<< fixed <<d;
+
+
 }
---
+++ global round 5/A-balanced_rating_changes.cpp
@@ -0,0 +1,85 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// #define ll long long
+// int main(){
+// 	ll n;
+// 	cin>>n;
+
+// 	ll arr[n],ans[n],sum=0;
+
+// 	for (int i = 0; i < n; ++i)
+// 	{
+// 		cin>>arr[i];
+// 			ans[i]=arr[i]/2;
+// 			sum+=ans[i];	
+// 	}
+
+
+// 	if(sum==0){
+// 	}
+// 	else if (sum<0){ 
+// 			int a = abs(sum);
+// 			int count=0;
+// 			for (int i = 0; i < n; ++i)
+// 			{
+				
+// 				if(count<a){
+// 					if(ans[i]>=0 && (arr[i]%2)!=0 && arr[i]>0){
+// 						ans[i]++;
+// 						count++;
+// 					}
+// 				}
+// 				else break;
+// 			}
+			
+// 	}
+// 	else if (sum>0){
+// 			int a = abs(sum);
+// 			int count=0;
+// 			for (int i = 0; i < n; ++i)
+// 			{
+				
+// 				if(count<a){
+// 					if(ans[i]<=0 && (arr[i]%2)!=0 && arr[i]<0){
+// 						ans[i]--;
+// 						count++;
+// 					}
+// 				}
+// 				else break;
+// 			}
+			
+// 	}
+
+// 	for (int i = 0; i < n; ++i)
+// 		{
+// 			cout<<ans[i]<<"\n";
+// 		}
+
+// }
+
+
+
+// TOURIST'S SOLUTION
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  int flag = 1;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    if (x % 2 == 0) {
+      cout << x / 2 << '\n';
+    } else {
+      cout << (x + flag) / 2 << '\n';
+      flag *= -1;
+    }
+  }
+  return 0;
+}
